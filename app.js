e25904b80cf983acdc3200caa1113d62868329c4
Piztle17 <mustaphaabiola17@gmail.com>
2023-10-02 12:16:36 -0700

    poup

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const btn = document.querySelector(".btn");
 const alert = document.querySelector(".alert");
+const icon = document.querySelector(".icon");
+const popup = document.querySelector(".outer");
 
 btn.addEventListener("click", () => {
     setInterval(() => {
         alert.style.display = "block";
-    }, 2000);
+    }, 1500);
+        popup.classList.add("openme");
+})
+icon.addEventListener("click", () => {
+    popup.classList.remove("openme");
 })
